15d9fabf505bf1b7cb9aa51f7f7f8e1d0c5fbb9a
Ricardo Jose Cedeño Mancilla <sjaimes16@outlook.com>
2024-07-24 17:16:10 -0500

    commit

--- src/app/directives/backgroud-color.directive.spec.ts
+++ src/app/directives/backgroud-color.directive.spec.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { BackgroudColorDirective } from './backgroud-color.directive';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 @Component({
   template:`
-    <h1 BackgroundColor="blue">Pruebas unitarias CIC</h1>
+    <h1 BackgroundColor="red">Pruebas unitarias CIC</h1>
     <div BackgroundColor="#ffbebe">prueba div</div>
+    <div BackgroundColor>prueba div</div>
+    <div>este div no utiliza la directiva</div>
     `
 })
 class HostComponent {};
@@ -31,4 +34,17 @@ fdescribe('BackgroudColorDirective', () => {
   it('should create', () =>{
     expect(component).toBeTruthy();
   })
+
+  it('should have three BackgroundColor elements', ()=>{
+    const elements = fixture.debugElement.queryAll(By.directive(BackgroudColorDirective));
+    expect(elements.length).toEqual(3);
+  })
+
+  it('should the elements be match with bgcolor', ()=>{
+    const elements = fixture.debugElement.queryAll(By.directive(BackgroudColorDirective));
+    expect(elements[0].nativeElement.style.backgroundColor).toEqual('red')
+    expect(elements[1].nativeElement.style.backgroundColor).toEqual('rgb(255, 190, 190)')
+    expect(elements[2].nativeElement.style.backgroundColor).toEqual('blue')
+  })
+
 });
